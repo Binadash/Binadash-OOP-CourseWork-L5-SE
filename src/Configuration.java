538b18cc6db91b243959c4a4ac5f616d99bdfbbf
Binada <binadasasmika@icloud.com>
2024-12-10 13:00:36 +0530

    initial commit

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,2 +1,17 @@
+// importing google gson
 import com.google.gson.Gson;
+
+// importing google gson builder
 import com.google.gson.GsonBuilder;
+
+// import java file reader
+import java.io.FileReader;
+
+// importing java file writer
+import java.io.FileWriter;
+
+// importing java exception handling
+import java.io.IOException;
+
+// creating the class file
+
