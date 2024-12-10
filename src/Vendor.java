1bc4d48069163a4f50e0308b853d489fcdad40a5
Binada <binadasasmika@icloud.com>
2024-12-10 20:50:02 +0530

    Vender class creation

--- src/Vendor.java
+++ src/Vendor.java
@@ -3,4 +3,11 @@ public class Vendor implements Runnable {
     private int ticketReleaseRate;
     private final TicketPool ticketPool;
 
+    public Vendor (int vendorID, int ticketReleaseRate, TicketPool ticketPool) {
+        this.vendorID = vendorID;
+        this.ticketReleaseRate = ticketReleaseRate;
+        this.ticketPool = ticketPool;
+    }
+
+
 }
