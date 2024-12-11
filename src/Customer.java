4d21e167678d4cb4d99bfcd87150d8373b19977b
Binada <binadasasmika@icloud.com>
2024-12-11 22:26:06 +0530

    commit

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,9 @@
 public class Customer implements Runnable {
     private int customerID;
     private int retrievalRate;
-    private int ticketpool;
+    private TicketPool ticketpool;
 
-    public Customer (int customerID, int retrievalRate, int ticketPool) {
+    public Customer (int customerID, int retrievalRate, TicketPool ticketPool) {
         this.customerID = customerID;
         this.retrievalRate = retrievalRate;
         this.ticketpool = ticketPool;
