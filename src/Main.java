44d1809a95ff3db0e3ca449f2ebd37b6f0a7a01e
Binada <binadasasmika@icloud.com>
2024-12-11 11:28:28 +0530

    logger corrections Main method creation

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,25 @@ public class Main{
 
         try {
             Logger.system("Welcome to the Ticketing System !");
+
+
+        }
+    }
+    private static int getPositiveInt (Scanner scanner, String prompt, String label) {
+        int value;
+        while (true) {
+            Logger.system(prompt);
+            try {
+                value = Integer.parseInt(scanner.nextLine());
+                Logger.userInput(label + value);
+                if (value > 0) {
+                    break;
+                } else {
+                    Logger.system ("Please Enter a Positive Number : ");
+                }
+            } catch (NumberFormatException e) {
+                Logger.system ("Invalid Integer. Please Enter A Positive Number : ")
+            }
         }
     }
 }
